729728498db2a92b9022e8ae95d256d9304d36bb
JonnyBoy9803 <jtha99@gmail.com>
2024-12-05 14:51:39 -0800

    extract raw values from json_contents dictionary

--- Code/maria_pdf.py
+++ Code/maria_pdf.py
@@ -1,10 +1,24 @@
 import json
 from reportlab.pdfgen import canvas
 
-pdf = canvas.Canvas("test1.pdf")
+#take in json file contents
+#https://www.geeksforgeeks.org/json-load-in-python/
+with open('test.json', 'r') as file:
+    json_contents = json.load(file)
 
-pdf.setTitle("Document Title")
+pdf = canvas.Canvas("test2.pdf")
 
-pdf.drawString(100, 750, "Hello World")
+title = json_contents.get("title", "Untitled")
+body_text = json_contents.get("bodyText", "Empty")
+table_data = json_contents.get("tableData", "Empty")
+chart_data = json_contents.get("chartData", "Empty")
+sub_head = json_contents.get("subhead", "Empty")
+details = json_contents.get("details", "Empty")
+recommendations = json_contents.get("recommendations", "Empty")
+request_and_response = json_contents.get("request_and_response_issues", "Empty")
+
+pdf.drawString(100, 750, title)
+
+pdf.drawString(100, 750, body_text)
 
 pdf.save()
