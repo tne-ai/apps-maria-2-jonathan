07bf86d6a000dbece68e99f3a678919aa5b18494
JonnyBoy9803 <jtha99@gmail.com>
2024-12-17 02:38:39 -0800

    changed height to 400-300

--- Code/maria_pdf.py
+++ Code/maria_pdf.py
@@ -105,7 +105,7 @@ def pdf_maker(content, file_name):
                 
                 chart_stream.seek(0)
 
-                img = Image(chart_stream, width = 500, height = 500)
+                img = Image(chart_stream, width = 400, height = 300)
 
                 story.append(img)
                 # chart_stream.close()
