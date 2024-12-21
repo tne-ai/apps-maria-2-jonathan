d3c5cd922daffb7d3fc5acbf0ee019b0b8ea023d
JonnyBoy9803 <jtha99@gmail.com>
2024-12-21 00:27:21 -0800

    init test_pdf_2.py

---
+++ Code/test_pdf_2.py
@@ -0,0 +1,28 @@
+import json
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from reportlab.platypus import Paragraph, Image, SimpleDocTemplate, Spacer, Table, TableStyle, PageBreak
+from reportlab.platypus import PageTemplate
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
+import matplotlib.pyplot as plt
+# from tne.TNE import TNE
+
+def pdf_maker(content, file_name):
+    #create pdf object, set bounds
+    pdf = SimpleDocTemplate(file_name, pagesize=letter)
+
+#take in file 
+#LOCALLY TEST
+with open('test.txt', 'r') as file:
+    txt_contents = file.read()
+
+#turn it into dictionary
+json_contents = json.loads(txt_contents)
+
+file_name = json_contents["document_filename"]
+content = json_contents
+
+#call pdf_maker method
+file = pdf_maker(content, file_name)
